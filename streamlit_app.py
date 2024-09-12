e6f046000302b338f3c802961b00adbb3fdda165
Savaliyaniks1 <150412744+Savaliyaniks1@users.noreply.github.com>
2024-09-13 00:10:32 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
-import streamlit as st,pandas as pd,numpy as np,seaborn as sns
+import streamlit as st
+import pandas as pd
+import numpy as np
+import seaborn as sns
 import plotly.express as px
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
